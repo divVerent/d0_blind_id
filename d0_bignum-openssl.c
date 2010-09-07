aeb58fa615ba48570b0fc84c02ba839b73853f7f
Rudolf Polzer <divverent@alientrap.org>
2010-09-07 19:52:24 +0200

    cannot check for BN_set_negative, so let's just #define it whenever BN_is_negative was missing

--- d0_bignum-openssl.c
+++ d0_bignum-openssl.c
@@ -49,8 +49,6 @@
 // for stupid OpenSSL versions in Mac OS X
 #ifndef BN_is_negative
 #define BN_is_negative(a) ((a)->neg != 0)
-#endif
-#ifndef BN_set_negative
 #define BN_set_negative(a,n) ((a)->neg = ((n) && !BN_is_zero(a)))
 #endif
 
